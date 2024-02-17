75bd98373d6a88f66f32de4dae21871531b4f9a4
mn3585 <158656925+mn3585@users.noreply.github.com>
2024-02-17 17:30:18 -0500

    webServer.py

--- webServer.py
+++ webServer.py
@@ -3,7 +3,6 @@ from socket import *
 # In order to terminate the program
 import sys
 
-
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -11,58 +10,34 @@ def webServer(port=13331):
     serverSocket.bind(("", port))
     serverSocket.listen(1)
 
-    # Fill in start
-
-    # Fill in end
-
     while True:
-        # Establish the connection
-
         print('Ready to serve...')
-        connectionSocket, addr = serverSocket.accept() # Fill in start -are you accepting connections?     #Fill in end
+        connectionSocket, addr = serverSocket.accept()
 
-    try:
-        message = connectionSocket.recv(1024).decode()  # Fill in start - a client is sending you a message #Fill in end
-        filename = message.split()[1]
+        try:
+            message = connectionSocket.recv(1024).decode()
+            filename = message.split()[1]
 
-        f = open(filename[1:], 'r', encoding='utf-8')  # fill in start #fill in end
+            with open(filename[1:], 'r', encoding='utf-8') as f:
+                # Prepare the HTTP response header
+                outputdata = 'HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\n\n'.encode()
 
-        # Prepare the HTTP response header
-        outputdata = 'HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\n\n'.encode()
+                for i in f:
+                    outputdata += i.encode('utf-8')
 
-        # Fill in start - append your html file contents
-        for i in f:
-            outputdata += i
-        # Fill in end
+            # Send the response (header + content)
+            connectionSocket.send(outputdata)
+            connectionSocket.close()
 
-        f.close()
+        except FileNotFoundError:
+            header = 'HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=UTF-8\n\n'.encode()
+            message = header + b"<html><body><h1>404 Not Found</h1></body></html>"
+            connectionSocket.send(message)
+            connectionSocket.close()
 
-        # Send the response (header + content)
-        # Fill in start
-        connectionSocket.send(outputdata)
-        # Fill in end
-
-        connectionSocket.close()
-
-    except Exception as e:
-        print("Error:", e)
-        # Send response message for invalid request due to the file not being found (404)
-        # Fill in start
-        header = 'HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=UTF-8\n\n'.encode()
-        errorMessage = header + b"<html><body><h1>404 Not Found</h1></body></html>"
-        connectionSocket.send(errorMessage)
-        # Fill in end
-
-        # Close client socket
-        # Fill in start
-    connectionSocket.close()
-        # Fill in end
-
-
-    # Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
-    # serverSocket.close()
-    # sys.exit()  # Terminate the program after sending the corresponding data
-
+        except Exception as e:
+            print("Error:", e)
+            connectionSocket.close()
 
 if __name__ == "__main__":
     webServer(13331)
