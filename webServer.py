31c021f6f4ef821b567b12c51087f8c5fde4edf7
mn3585 <158656925+mn3585@users.noreply.github.com>
2024-02-17 20:50:16 -0500

    webServer.py

--- webServer.py
+++ webServer.py
@@ -1,8 +1,8 @@
+
 # import socket module
 from socket import *
 # In order to terminate the program
 import sys
-
 
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -10,68 +10,32 @@ def webServer(port=13331):
     # Prepare a server socket
     serverSocket.bind(("", port))
     serverSocket.listen(1)
-    while True:
-        # Establish the connection
 
-        print('Ready to serve...')
+    while True:
+        #print('Ready to serve...')
         connectionSocket, addr = serverSocket.accept()
 
         try:
             message = connectionSocket.recv(1024).decode()
             filename = message.split()[1]
 
-            # opens the client requested file.
-            # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-            #f = open(filename[1:], 'r', encoding='utf-8')
-            f = open(filename[1:], 'r')
-            #fileContent = f.read()
+            with open(filename[1:], 'r', encoding='utf-8') as f:
+                # Prepare the HTTP response header
+                outputdata = 'HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\r\n\r\n'.encode()
 
+                for i in f:
+                    outputdata += i
+                    #outputdata += i.encode('utf-8')
 
-                     # This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?
-                     # Fill in start
-            #header = b"HTTP 1.1/ 200 OK\Content-Type: text/html; charset=UTF-8\r\n"
-            header = r"HTTP 1.1/ 200 OK\Content-Type: text/html; charset=UTF-8\r\n"
-            # Content-Type is an example on how to send a header as bytes. There are more!
-            outputdata=b"Content-Type: text/html; charset=UTF-8\r\n"
-
-            # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
-
-            # Fill in end
-
-            for i in f:  # for line in file
-
-            # Fill in start - append your html file contents #Fill in end
-                header += i
-
-            # Send the content of the requested file to the client (don't forget the headers you created)!
-            # Send everything as one send command, do not send one line/item at a time!
-
-            # Fill in start
-            connectionSocket.send(header.encode())
-            f.close()
-            # Fill in end
-
-            connectionSocket.close()  # closing the connection socket
+            # Send the response (header + content)
+            connectionSocket.send(outputdata)
+            connectionSocket.close()
 
         except Exception as e:
-    # Send response message for invalid request due to the file not being found (404)
-    # Remember the format you used in the try: block!
-    # Fill in start
-            header = 'HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=UTF-8\r\n'
-            connectionSocket.send(header.encode())
-    # Fill in end
-
-    # Close client socket
-
-    # Fill in start
-        connectionSocket.close()
-    # Fill in end
-
-    # Commenting out the below (some use it for local testing). It is not required for Gradescope, and some students have moved it erroneously in the While loop.
-    # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
-    # serverSocket.close()
-    # sys.exit()  # Terminate the program after sending the corresponding data
-
+            header = 'HTTP/1.1 404 Not Found\nContent-Type: text/html; charset=UTF-8\r\n\r\n'.encode()
+            #message = header + b"<html><body><h1>404 Not Found</h1></body></html>"
+            connectionSocket.send(header)
+            connectionSocket.close()
 
 if __name__ == "__main__":
     webServer(13331)
