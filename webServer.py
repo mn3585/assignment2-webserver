1356c217318032293530ff6eb5fc3ba46409d843
mn3585 <158656925+mn3585@users.noreply.github.com>
2024-02-17 21:14:47 -0500

    webServer.py

--- webServer.py
+++ webServer.py
@@ -18,13 +18,16 @@ def webServer(port=13331):
             message = connectionSocket.recv(1024).decode()
             filename = message.split()[1]
 
-            with open(filename[1:], 'r', encoding='utf-8') as f:
+            with open(filename[1:], 'r') as f:
+            #with open(filename[1:], 'r', encoding='utf-8') as f:
                 # Prepare the HTTP response header
                 outputdata = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n'.encode()
 
                 for i in f:
-                    outputdata += i
-                    #outputdata += i.encode('utf-8')
+                    #outputdata += i
+                    outputdata += i.encode('utf-8')
+
+                outputdata +="\r\n".encode()
 
             # Send the response (header + content)
             connectionSocket.send(outputdata)
@@ -32,6 +35,7 @@ def webServer(port=13331):
 
         except Exception as e:
             header = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n'.encode()
+            #message = header + b"<html><body><h1>404 Not Found</h1></body></html>"
             connectionSocket.send(header)
             connectionSocket.close()
 
